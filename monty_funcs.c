2cb9dd81f5eec12bce658dad71d1d1293339c61f
Dridi Chaith <dridi.chaith@gmail.com>
2023-11-15 15:12:09 +0100

    first function

---
+++ monty_funcs.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * read_file - read the file lines
+ * @filename: filename
+ * @stack: stack
+ *
+ * Return: nothing
+ */
+void read_file(char *filename, stack_t **stack)
+{
+	size_t length = 0;
+	ssize_t line_read = 0;
+
+	/*Open the file*/
+	global_vars->file = fopen(filename, "r");
+	if (global_vars->file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	/*read the lines*/
+	while ((line_read = getline(&global_vars->buffer, &length, global_vars->file)) != -1)
+	{
+		/**/
+		line = parse_line()
+	}
+}
