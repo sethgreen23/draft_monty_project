1753743225397e611a00edf75ad975654bed0ff1
loayalsaid1 <l.programming555@gmail.com>
2023-11-16 15:32:12 +0200

    Add div

--- monty.h
+++ monty.h
@@ -68,6 +68,9 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
 void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+
+
 
 
 extern vars_global global_vars;
--- parse_func1.c
+++ parse_func1.c
@@ -53,3 +53,30 @@ void sub(stack_t **stack, unsigned int line_number)
 	(*stack)->prev->n = (*stack)->prev->n - (*stack)->n;
 	pop(stack, line_number);
 }
+
+/**
+ * div - div divides the second top element of the stack by the top element of the stack.
+ * and operate pop, unless top == zero
+ * @stack: stack
+ * @line_number: line_number
+ *
+ * Return: nothing
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	int result = 0;
+	if (*stack == NULL || (*stack)->prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = (*stack)->prev->n / (*stack)->n;
+	(*stack)->prev->n = result;
+	pop(stack, line_number);
+}
+
