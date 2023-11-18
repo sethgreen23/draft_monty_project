6e4c9f73d2f55e49c514fce98bc1068843ee5db0
Dridi Chaith <dridi.chaith@gmail.com>
2023-11-18 13:26:01 +0100

    setting changing mode functions

--- parse_func3.c
+++ parse_func3.c
@@ -12,4 +12,30 @@ void stack(stack_t **stack, unsigned int line_number)
 {
 	if (mode == 1)
 		return;
+	mode = 1;
+}
+
+/**
+ * queue - prints the string starting at the top of the stack, followed
+ * by a new line, while the interger being treated as an ascii value
+ * @stack: stack
+ * @line_number: line_number
+ *
+ * Return: nothing
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+	if (mode == 2)
+		return;
+	mode = 2;
+}
+
+/**
+ * get_mode - get the current mode
+ *
+ * Return: mode
+ */
+int get_mode(void)
+{
+	return (mode);
 }
